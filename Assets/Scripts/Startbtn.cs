83f687fe0bd6764eada84b4b9f015ef7b0da665b
Rishikesh-kumar-7258 <rishi7258prince@gmail.com>
2021-10-30 17:22:26 +0530

    Made the Welcome scene

---
+++ Assets/Scripts/Startbtn.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Startbtn : MonoBehaviour
+{
+    // When user clicks on the start button on welcome screen this function is triggered
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
